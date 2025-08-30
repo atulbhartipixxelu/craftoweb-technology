6b83aa294850ecb8134e21d48345cb2d10c3b0f2
pixxelu16 <Rivaan@176001>
2025-08-30 11:28:35 +0530

    nobitea

---
+++ src/components/HomePage/ServiceSection/service.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Styles from '../ServiceSection/service.module.css'
+function Service() {
+
+    return (
+        <div className={'ServiceSection'}>
+           
+        <div className={Styles.free}>
+            Styles
+        </div>
+        </div>
+    )
+}
+
+
+export default Service;
